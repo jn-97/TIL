e5b3cc4f9f12cc27e4b11c4ef6879016fc2e5ea2
jn-97 <kjo5395@gmail.com>
2022-10-18 18:25:19 +0900

    10-18과제

---
+++ WEB/Django/project_14/articles/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ArticleForm
+from .forms import CommentForm
+from .models import Article
+
+# Create your views here.
+def create(request):
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            messages.success(request, "글 작성이 완료되었습니다.")
+            return redirect("articles:index")
+    else:
+        article_form = ArticleForm()
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "articles/create.html", context)
+
+
+def index(request):
+    articles = Article.objects.order_by("-pk")
+    context = {
+        "articles": articles,
+    }
+    return render(request, "articles/index.html", context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm()
+    context = {
+        "article": article,
+        "comments": article.comment_set.all(),
+        "comment_form": comment_form,
+    }
+    return render(request, "articles/detail.html", context)
+
+
+def comment_create(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    return redirect("articles:detail", article.pk)
