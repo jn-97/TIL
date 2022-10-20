361ea073fc240f48d1f2ea4f31cd54a8ec5dcdfa
jn-97 <kjo5395@gmail.com>
2022-10-20 13:45:36 +0900

    2022-10-19

---
+++ WEB/Django/project_14/accounts/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import redirect, render
+from accounts.forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+
+# Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:login")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("articles:index")
+    else:
+        form = AuthenticationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    messages.warning(request, "로그아웃 하였습니다.")
+    return redirect("articles:index")
+
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/detail.html", context)
--- WEB/Django/project_14/pjt/settings.py
+++ WEB/Django/project_14/pjt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts",
     "articles",
     "bootstrap5",
     "django.contrib.admin",
@@ -125,3 +126,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
