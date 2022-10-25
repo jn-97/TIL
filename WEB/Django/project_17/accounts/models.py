cb0c663d55132e78bf9ee86f3ef5d75c4e5c724e
jn-97 <kjo5395@gmail.com>
2022-10-25 17:24:37 +0900

    2022-10-25

---
+++ WEB/Django/project_17/accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to="images/profile", blank=True)
+    pass
+
+    def full_name(self):
+        return f"{self.last_name}{self.first_name}"
